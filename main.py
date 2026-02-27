a585e4901d7e8d5aaf33cc7808edaaab90df6e05
samadriaz9 <samadriaz9@gmail.com>
2026-02-27 10:09:06 +0500

    Filteration_flask_configured

--- main.py
+++ main.py
@@ -15,16 +15,7 @@ import time
 try:
     # First: move down until limit switch on P0 (PCF8574) is pressed
     filteration_flask_config()
-    # Then run fixed-step movements
-    #Filteration_flask_up(1000)   # 1000 steps up
-    #Filteration_flask_down(100)  # 500 steps down
-    Filteration_flask_up(3500)   # 1000 steps up
-    time.sleep(10)
-    Filteration_flask_down(900)
-    time.sleep(10)
-    for i in range(5):
-        Filteration_flask_down(520)
-        time.sleep(5)   # 500 steps down
+    Filteration_flask_up(1150)   # 1000 steps up
 finally:
     cleanup()
     GPIO.cleanup()
