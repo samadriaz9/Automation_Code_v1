cd7f53335a3eff12cc357e1aa2b17ddb026017c8
samadriaz9 <samadriaz9@gmail.com>
2026-03-12 13:20:09 +0500

    demo

--- main.py
+++ main.py
@@ -66,62 +66,113 @@ from media_dispensor import (
     Media_dispensor_up,
     Media_dispensor_down,
 )
+
+from solinoid_value import solenoid_valve_on, solenoid_valve_off, solenoid_valve
 import RPi.GPIO as GPIO
 import time
 
 try:
+    solenoid_valve(0.1)
+    x = input ('Enter 1: ')
+    
+    Media_dispensor_home()
+    x= input ('Enter 2: ')
+    
+    Media_dispensor_down(3500)
+    
+    x = input ('Enter 3: ')
+    Media_dispensor_up(800)
+    
+    x = input ('Enter 4: ')
+    Consumable_down(400)
+    x = input ('Enter 5: ')
+    Consumable_up(400)
+    
+    x = input ('Enter 5: ')
+    petri_dishes_home()
+    petri_dishes_down(1870)
+    
+    Camera_home()
+    Camera_down(2500)
+    
+    
+    x= input ('Enter 6: ')
     #media pad + petri dish
-    #suction_pump_home()   # step 1
-    #suction_pump_up(250)  # step 2
-    #Consumable_up(290)    # step 3
-
-     #✅ start pump
-    #suction_pump_on(100)
-    #time.sleep(1)
-    # ✅ move stepper while pump is running
-    #suction_pump_up(3110)
-
-    # ✅ stop pump immediately after motion
-    #suction_pump_off()
+    suction_pump_home()   # step 1
+    suction_pump_up(250)  # step 2
     
+    x = input ("Enter 7: ")
+    filteration_unit_config()
+    filteration_flask_config()
+    Filteration_flask_up(1150)
     
+    x = input ("Enter 7: ")
+    
+    Consumable_up(290)    # step 3
+    suction_pump_on(100)
+    time.sleep(1)
+    suction_pump_up(3110)
+    suction_pump_off()
+    Consumable_down(290)
+    
+    x = input ("Enter 8: ")
+    
+    petri_dishes_down(800)
+    Media_dispensor_up(520)
+    petri_dishes_up(800)
+    
+    x = input ("Enter 9: ")
     #  Filter Paper
-    #suction_pump_home()
-    #suction_pump_up(400)
-    #Consumable_up(310)
-    #suction_pump_on(100)
-    #time.sleep(1)
-    #Consumable_down(310)
-    #suction_pump_up(1200)
-    #suction_pump_off()
+    Consumable_up(290)
+    suction_pump_home()
+    suction_pump_up(420)
+    Consumable_up(310)
+    suction_pump_on(35)
+    time.sleep(2)
+    Consumable_down(310)
+    suction_pump_up(1220)
+    suction_pump_off()
+    Consumable_down(290)
     
-    #filteration_suction_pump_on(90)
-    #time.sleep(2)
-    #filteration_suction_pump_off()
+    x = input ("Enter 10: ")
+    filteration_suction_pump_on(90)
+    time.sleep(2)
+    filteration_suction_pump_off()
     
-    #filteration_unit_config()
-    #filteration_flask_config()
-    #Filteration_flask_up(32)
-    #Filteration_unit_up(850)
-    
-    #filteration_suction_pump_on(90)
-    #time.sleep(2) 
-    #filteration_suction_pump_off()
-    
-    #filteration_unit_config()
-    #Filteration_flask_up(1150)
+    filteration_unit_config()
+    filteration_flask_config()
+    Filteration_flask_up(32)
+    Filteration_unit_up(850)
     
     
+    #solinoid
+    x= input ("Enter 11: ")
+    solenoid_valve(2)
+    
+    x = input ("Enter 12: ")
+    filteration_suction_pump_on(90)
+    time.sleep(5) 
+    filteration_suction_pump_off()
+    
+    x = input ("Enter 13: ")
+    filteration_unit_config()
+    Filteration_flask_up(1150)
     
     
-    #suction_pump_on(100)
-    #time.sleep(1)
-    #suction_pump_off()
+    x= input ("Enter 12: ") #carefully observe
+    suction_pump_on(100)
+    time.sleep(5)
+    suction_pump_up(1820)
+    suction_pump_off()
+    time.sleep(3)
+    suction_pump_down(1820)
     
-    #petri_dishes_home()
-    #Camera_home()
-    #Media_dispensor_home()
-    consumable_cleanup()
+    x = input ("Enter 13: ")
+    petri_dishes_home()
+    Camera_up(500)
+    Camera_down(500)
+    
+
 finally:
     # Clean up all modules
     filteration_cleanup()
