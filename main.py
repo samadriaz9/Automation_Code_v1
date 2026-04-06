b2a97da653a327b717c485672f17647e3d92ac56
Samad Riaz <samadriaz9@gmail.com>
2026-04-06 14:26:55 +0500

    put in trash

--- main.py
+++ main.py
@@ -200,9 +200,9 @@ try:
     suction_pump_down(930)
     upper_suction_pump_off()
     suction_pipe_up(500)
-    for i in range(100):
-        suction_pump_up(15)
-        suction_pump_down(15)
+    for i in range(200):
+        suction_pump_up(5)
+        suction_pump_down(5)
         time.sleep(0.01)
     time.sleep(2)
 
