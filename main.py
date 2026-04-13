fa089cba2aac9291ba0ff947b6198ac862f6e686
samadriaz9 <samadriaz9@gmail.com>
2026-04-13 10:23:45 +0500

    mediapad_to_trash

--- main.py
+++ main.py
@@ -185,7 +185,6 @@ signal.signal(signal.SIGTERM, _on_sigterm)
 atexit.register(shutdown_all)
 
 try:
-    x = input ('Enter to home all modules: ')
     x = input ('Enter to put media pad in trash: ')
     incubator_lid_home()
     petri_dishes_home()
@@ -397,11 +396,6 @@ try:
         suction_pump_down(120)
         time.sleep(0.01)
     time.sleep(2)
-
-
-
-
-
 
 
 except KeyboardInterrupt:
