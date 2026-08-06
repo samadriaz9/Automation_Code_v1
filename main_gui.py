d0a9457c088c32c32fce6a773b77415881394d01
samadriaz9 <samadriaz9@gmail.com>
2026-08-05 22:51:45 -0700

    sterilization_added

--- main_gui.py
+++ main_gui.py
@@ -1876,10 +1876,10 @@ class ExperimentApp:
     def _run_sterilize_suction_worker(self):
         # --- tweak these ---
         bts_seconds = 5
-        pump_up_steps = 600
-        pipe_up_steps = 600
-        jog_count = 10
-        jog_steps = 10
+        pump_up_steps = 570
+        pipe_up_steps = 900
+        jog_count = 2
+        jog_steps = 2
         # -------------------
         try:
             self.write_log(f"Sterilize_Suction: BTS ON for {bts_seconds} seconds (I2C P2)")
